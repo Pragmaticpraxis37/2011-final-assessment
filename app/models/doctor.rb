33f4c88e2432a8f9bd3a73d8bb2307290962edf0
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-08 10:15:59 -0700

    Craete first Doctor model method tests passes

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,8 @@
 class Doctor < ApplicationRecord
-   has_many :doctor_surgeries
-   has_many :surgeries, through: :doctor_surgeries
+  has_many :doctor_surgeries
+  has_many :surgeries, through: :doctor_surgeries
+
+  def self.years_practicing
+    order(years_practiced: :desc)
+  end
 end
