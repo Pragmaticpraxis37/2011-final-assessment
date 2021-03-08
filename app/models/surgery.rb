bd62ce6f81176291eba71075901407be9543af81
Adam Bowers <adam.j.bowers@gmail.com>
2021-03-08 10:34:25 -0700

    Create two method tests, most and least for doctors

--- app/models/surgery.rb
+++ app/models/surgery.rb
@@ -1,4 +1,8 @@
 class Surgery < ApplicationRecord
-   has_many :doctor_surgeries
-   has_many :doctors, through: :doctor_surgeries
+  has_many :doctor_surgeries
+  has_many :doctors, through: :doctor_surgeries
+
+  def all_surgical_doctors
+    doctors 
+  end
 end
--- spec/models/surgery_spec.rb
+++ spec/models/surgery_spec.rb
@@ -11,11 +11,11 @@ describe Surgery, type: :model do
       @doctor_1 = Doctor.create!(name: "Doc 1", years_practiced: 5, university: "Big U")
       @doctor_2 = Doctor.create!(name: "Doc 2", years_practiced: 10, university: "Little U")
       @doctor_3 = Doctor.create!(name: "Doc 3", years_practiced: 15, university: "Middle U")
-      @surgery_1 = @doctor_1.surgeries.create!(title: "Heart", day_of_week: "Wednesday", operating_room: "1")
-      @surgery_1 = @doctor_2.surgeries.create!(title: "Heart", day_of_week: "Wednesday", operating_room: "1")
+      @surgery_1 = Surgery.create!(title: "Heart", day_of_week: "Wednesday", operating_room: "1")
       @surgery_2 = @doctor_2.surgeries.create!(title: "Stomach", day_of_week: "Monday", operating_room: "3")
       @surgery_3 = @doctor_2.surgeries.create!(title: "Foot", day_of_week: "Friday", operating_room: "2")
-
+      @surg_1_doc_1 = DoctorSurgery.create!(doctor_id:@doctor_1.id, surgery_id: @surgery_1.id)
+      @surg_1_doc_2 = DoctorSurgery.create!(doctor_id:@doctor_2.id, surgery_id: @surgery_1.id)
     end
 
     it "#all_surgical_doctors" do
@@ -24,6 +24,14 @@ describe Surgery, type: :model do
                 end
 
       expect(expected).to eq(["Doc 1", "Doc 2"])
+    end
+
+    it "#least_experienced_doctor" do
+      expect(@surgery_1.least_experienced_doctor).to eq(5)
+    end
+
+    it "#most_experienced_doctor" do 
+      expect(@surgery_1.least_experienced_doctor).to eq(10)
     end
   end
 end
